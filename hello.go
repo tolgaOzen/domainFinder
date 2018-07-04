eaf844f0a540da2240a178934e8697ad43027a81
Tolga Ozen <39353278+tolgaOzen@users.noreply.github.com>
2018-07-04 22:07:39 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,40 +1,40 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
+// import (
+// 	"fmt"
+// 	"net"
+// )
 
-func handler(c net.Conn, ch chan string) {
-	ch <- c.RemoteAddr().String()
-	c.Write([]byte("ok"))
-	c.Close()
-}
+// func handler(c net.Conn, ch chan string) {
+// 	ch <- c.RemoteAddr().String()
+// 	c.Write([]byte("ok"))
+// 	c.Close()
+// }
 
-func logger(ch chan string) {
-	for {
-		fmt.Println(<-ch)
-	}
-}
+// func logger(ch chan string) {
+// 	for {
+// 		fmt.Println(<-ch)
+// 	}
+// }
 
-func server(l net.Listener, ch chan string) {
-	for {
-		c, err := l.Accept()
-		if err != nil {
-			continue
-		}
-		go handler(c, ch)
-	}
-}
+// func server(l net.Listener, ch chan string) {
+// 	for {
+// 		c, err := l.Accept()
+// 		if err != nil {
+// 			continue
+// 		}
+// 		go handler(c, ch)
+// 	}
+// }
 
-func main() {
-	l, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		panic(err)
-	}
-	ch := make(chan string)
-	go server(l, ch)
-	logger(ch)
+// func main() {
+// 	l, err := net.Listen("tcp", ":8080")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	ch := make(chan string)
+// 	go server(l, ch)
+// 	logger(ch)
 
-	//time.Sleep(10 * time.Second)
-}
+// 	//time.Sleep(10 * time.Second)
+// }
